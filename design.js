d4c9f17f121e01e688458033ea1680f3399846a5
HemaAmruthaDasari <137883961+HemaAmruthaDasari@users.noreply.github.com>
2023-07-28 16:01:55 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,13 @@
+ReactDOM.render(
+    <div>
+    <h1>Biography</h1>
+    <img height="300px" width="200px" src="https://upload.wikimedia.org/wikipedia/commons/5/58/CheHigh.jpg"/>
+    <h1>Che Guevara</h1>
+    <h2>14 june 1928 - 9 october 1967</h2>
+    <h2>Minister Of Industries Of Cuba</h2>
+    <h2>President Of National Bank Of Cuba</h2>
+    <h3>Ernesto Guevara de la Serna, known as Che Guevara, was born on 14 June 1928 in Rosario, Argentina into a middle-class family. He studied medicine at Buenos Aires University and during this time travelled widely in South and Central America. The widespread poverty and oppression he witnessed, fused with his interest in Marxism, convinced him that the only solution to South and Central America's problems was armed revolution.</h3>
+    <h3>In 1954 he went to Mexico and the following year he met Cuban revolutionary leader Fidel Castro. Guevara joined Castro's '26th July Movement' and played a key role in the eventual success of its guerrilla war against Cuban dictator Fulgencio Batista.</h3>
+    <h3>With US assistance, the Bolivian army captured Guevara and his remaining fighters. He was executed on 9 October 1967 in the Bolivian village of La Higuera and his body was buried in a secret location. In 1997 his remains were discovered, exhumed and returned to Cuba, where he was reburied.</h3>
+    </div>
+    ,document.getElementById("bio"));
